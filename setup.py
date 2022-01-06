cc79950bed796b880371f2ee4e0c95a85c55b60c
Eddie2054 <96389699+Eddie2054@users.noreply.github.com>
2022-01-07 00:39:13 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,4 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
